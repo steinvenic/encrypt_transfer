2c0439ea32dbf5e377b6eb04d84a49959dd765b2
Yang Xuetong <yangxuetong@cgprintech.com>
2022-07-28 16:56:32 +0800

    待完流上传

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,10 @@
     <div class="el-upload__text">
       Drop file here or <em>click to upload</em>
     </div>
+
+
+
+
   </el-upload>
 </template>
 
@@ -19,6 +23,7 @@
 const fs = window.require('fs')
 const path = window.require('path')
 const FileCrypt = window.require("file-aes-crypt");
+const axios = require('axios');
 
 export default {
   data() {
@@ -26,16 +31,18 @@ export default {
   },
   methods: {
     //上传文件的事件
-    uploadFile(item) {
-      const fc = new FileCrypt('Aaa00123');
-      fc.encrypt("package.json", "package.json-kkkkkk");
-      console.log('文件上传中........')
-      //上传文件的需要formdata类型;所以要转
-      let fileName;
-      fileName = path.basename(item.file.path)
-      const axios = require('axios');
-      fs.readFile(item.file.path, {encoding: "utf-8"}, async function (err, fr) {
-        //readFile回调函数
+    async uploadFile(item) {
+      this.$message({message:"文件加密中...",duration:0},);
+      const secret = this.uuid()
+      const fc = new FileCrypt(secret);
+
+      let filePath = item.file.path;
+      let fileName = path.basename(filePath)
+      let encFileName = fileName + ".enc"
+      await fc.encrypt(filePath, encFileName);
+
+      this.$message("开始上传...");
+      fs.readFile(encFileName, {encoding: "utf-8"}, async function (err, fr) {
         if (err) {
           console.log(err);
         } else {
@@ -44,6 +51,18 @@ export default {
         }
       });
     },
+    uuid() {
+      var s = [];
+      var hexDigits = "0123456789abcdef";
+      for (var i = 0; i < 32; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+      }
+      s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+      s[8] = s[13] = s[18] = s[23];
+      var uuid = s.join("");
+      return uuid;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-import AES from './utils/AES';
 
 const app = createApp(App)
-app.config.globalProperties.$aes = AES;
 
 app.use(ElementPlus)
 app.mount('#app')
