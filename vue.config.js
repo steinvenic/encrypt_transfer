ee227be88bfdfdd2162dfa8081087faa92e66e6f
Yang Xuetong <yangxuetong@cgprintech.com>
2022-08-03 10:03:14 +0800

    指定file-aes-crypt

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,14 @@ module.exports = {
     resolve: {
       fallback: { path: require.resolve("path-browserify") },
     },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      // List native deps here if they don't work
+      externals: ['file-aes-crypt'],
+      // If you are using Yarn Workspaces, you may have multiple node_modules folders
+      // List them all here so that VCP Electron Builder can find them
+      nodeModulesPath: ['../../node_modules', './node_modules']
+    }
   }
 }
